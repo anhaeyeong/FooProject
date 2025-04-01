394a94481f356692839b4a2b5344310dfa993a12
kofeeel <70192238+kofeeel@users.noreply.github.com>
2025-04-01 21:21:31 +0900

    c
    c

--- Project_R/250304_WinAPI/MissileFactory.cpp
+++ Project_R/250304_WinAPI/MissileFactory.cpp
@@ -1,86 +1,86 @@
-//#include "MissileFactory.h"
-//#include "MainGame.h"
-//
-//// �̻��� ���丮 �Ҹ���
-//MissileFactory::~MissileFactory()
-//{
-//   for (Missile* missile : vecMissiles)
-//       delete missile;
-//   vecMissiles.clear();
-//}
-//
-//// �̻��� �߰�
-//void MissileFactory::AddMissile(MissileType type)
-//{
-//   Missile* missile = CreateMissile(type);
-//   if (missile)
-//   {
-//       vecMissiles.push_back(missile);
-//       missile->Notice();
-//       
-//   }
-//   else
-//   {
-//       std::cerr << "Failed to add missile: " << static_cast<int>(type) << std::endl;
-//   }
-//}
-//
-//
-//
-//void MissileFactory::Init()  
-//{  
-//  PlayerMissileFactory::GetInstance();
-//  EnemyMissileFactory::GetInstance();  
-//}
-//
-//void MissileFactory::Release()
-//{
-//   PlayerMissileFactory::GetInstance()->ReleaseInstance();
-//   EnemyMissileFactory::GetInstance()->ReleaseInstance();
-//}
-//
-//void MissileFactory::Update() {
-//    for (Missile* missile : vecMissiles) {
-//        if (missile) {
-//            missile->Update();
-//        }
-//    }
-//}
-//
-//void MissileFactory::Render(HDC hdc) {
-//    for (Missile* missile : vecMissiles) {
-//        if (missile) {
-//            missile->Render(hdc);
-//        }
-//    }
-//}
-//
-//// MissileFactory.cpp
-//Missile* PlayerMissileFactory::CreateMissile(MissileType type)
-//{
-//   Missile* missile = 0;
-//   switch (type)
-//   {
-//   case MissileType::NORMAL:
-//       missile = new NormalMissile(MissileOwner::PLAYER);
-//       break;
-//   case MissileType::SIGN:
-//       missile = new SignMissile(MissileOwner::PLAYER);
-//       break;
-//   case MissileType::LAZER:
-//       missile = new LazerMissile(MissileOwner::PLAYER);
-//       break;
-//   default:
-//       std::cerr << "Unknown MissileType: " << static_cast<int>(type) << std::endl;
-//       break;
-//   }
-//   if (!missile)
-//       std::cerr << "Missile creation failed for type: " << static_cast<int>(type) << std::endl;
-//   return missile;
-//}
-//
-//
-//Missile* EnemyMissileFactory::CreateMissile(MissileType type)
-//{
-//   return new NormalMissile(MissileOwner::ENEMY);
-//}
+#include "MissileFactory.h"
+#include "MainGame.h"
+
+// �̻��� ���丮 �Ҹ���
+MissileFactory::~MissileFactory()
+{
+   for (Missile* missile : vecMissiles)
+       delete missile;
+   vecMissiles.clear();
+}
+
+// �̻��� �߰�
+void MissileFactory::AddMissile(MissileType type)
+{
+   Missile* missile = CreateMissile(type);
+   if (missile)
+   {
+       vecMissiles.push_back(missile);
+       missile->Notice();
+       
+   }
+   else
+   {
+       std::cerr << "Failed to add missile: " << static_cast<int>(type) << std::endl;
+   }
+}
+
+
+
+void MissileFactory::Init()  
+{  
+  PlayerMissileFactory::GetInstance();
+  EnemyMissileFactory::GetInstance();  
+}
+
+void MissileFactory::Release()
+{
+   PlayerMissileFactory::GetInstance()->ReleaseInstance();
+   EnemyMissileFactory::GetInstance()->ReleaseInstance();
+}
+
+void MissileFactory::Update() {
+    for (Missile* missile : vecMissiles) {
+        if (missile) {
+            missile->Update();
+        }
+    }
+}
+
+void MissileFactory::Render(HDC hdc) {
+    for (Missile* missile : vecMissiles) {
+        if (missile) {
+            missile->Render(hdc);
+        }
+    }
+}
+
+// MissileFactory.cpp
+Missile* PlayerMissileFactory::CreateMissile(MissileType type)
+{
+   Missile* missile = 0;
+   switch (type)
+   {
+   case MissileType::NORMAL:
+       missile = new NormalMissile(MissileOwner::PLAYER);
+       break;
+   case MissileType::SIGN:
+       missile = new SignMissile(MissileOwner::PLAYER);
+       break;
+   case MissileType::LAZER:
+       missile = new LazerMissile(MissileOwner::PLAYER);
+       break;
+   default:
+       std::cerr << "Unknown MissileType: " << static_cast<int>(type) << std::endl;
+       break;
+   }
+   if (!missile)
+       std::cerr << "Missile creation failed for type: " << static_cast<int>(type) << std::endl;
+   return missile;
+}
+
+
+Missile* EnemyMissileFactory::CreateMissile(MissileType type)
+{
+   return new NormalMissile(MissileOwner::ENEMY);
+}
--- Project_R/250304_WinAPI/MissileFactory.h
+++ Project_R/250304_WinAPI/MissileFactory.h
@@ -1,35 +1,35 @@
-//#pragma once
-//#include "Missile.h"
-//#include "Singleton.h"
-//
-//class MissileFactory
-//{
-//protected:
-//    vector<Missile*> vecMissiles; // �̻��� ����
-//    MissileOwner ownerType;            // �÷��̾� or �� ����
-//
-//public:
-//    MissileFactory(MissileOwner owner) : ownerType(owner) {}
-//    virtual ~MissileFactory();
-//
-//    void AddMissile(MissileType type);
-//    static void Init();
-//    static void Release();
-//    void Update();
-//    void Render(HDC hdc);
-//    virtual Missile* CreateMissile(MissileType type) = 0;
-//};
-//
-//class PlayerMissileFactory : public MissileFactory, public Singleton<PlayerMissileFactory>
-//{
-//public:
-//    PlayerMissileFactory() : MissileFactory(MissileOwner::PLAYER) {}
-//    virtual Missile* CreateMissile(MissileType type) override;
-//};
-//
-//class EnemyMissileFactory : public MissileFactory, public Singleton<EnemyMissileFactory>
-//{
-//public:
-//    EnemyMissileFactory() : MissileFactory(MissileOwner::ENEMY) {}
-//    virtual Missile* CreateMissile(MissileType type) override;
-//};
+#pragma once
+#include "Missile.h"
+#include "Singleton.h"
+
+class MissileFactory
+{
+protected:
+    vector<Missile*> vecMissiles; // �̻��� ����
+    MissileOwner ownerType;            // �÷��̾� or �� ����
+
+public:
+    MissileFactory(MissileOwner owner) : ownerType(owner) {}
+    virtual ~MissileFactory();
+
+    void AddMissile(MissileType type);
+    static void Init();
+    static void Release();
+    void Update();
+    void Render(HDC hdc);
+    virtual Missile* CreateMissile(MissileType type) = 0;
+};
+
+class PlayerMissileFactory : public MissileFactory, public Singleton<PlayerMissileFactory>
+{
+public:
+    PlayerMissileFactory() : MissileFactory(MissileOwner::PLAYER) {}
+    virtual Missile* CreateMissile(MissileType type) override;
+};
+
+class EnemyMissileFactory : public MissileFactory, public Singleton<EnemyMissileFactory>
+{
+public:
+    EnemyMissileFactory() : MissileFactory(MissileOwner::ENEMY) {}
+    virtual Missile* CreateMissile(MissileType type) override;
+};
